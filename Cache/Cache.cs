22ba09a4d17581c8e1abced296beefe2407c0800
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2021-02-21 13:13:09 +0100

    usings

--- Cache/Cache.cs
+++ Cache/Cache.cs
@@ -1,62 +1,61 @@
-﻿using System;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Threading.Tasks;
 
 namespace Cache
 {
-    public class Cache
+    public static class TwowayRedis
     {
-        public static class TwowayRedis
+        public static async Task<string> AddAndOrGetKeyAsync(this IDistributedCache cache, KeyType type, string inputKey)
         {
-            public static async Task<string> AddAndOrGetKeyAsync(this IDistributedCache cache, KeyType type, string inputKey)
-            {
-                string outputKey = await cache.GetStringAsync(StringHelper.AddPrefix(type, inputKey));
+            string outputKey = await cache.GetStringAsync(StringHelper.AddPrefix(type, inputKey));
 
-                if (string.IsNullOrEmpty(outputKey) == false)
-                    return outputKey;
+            if (string.IsNullOrEmpty(outputKey) == false)
+                return outputKey;
 
-                return await AddKeyToCacheAndReturn(cache, type, inputKey);
-            }
-            public static async Task<string> AddKeyToCacheAndReturn(IDistributedCache cache, KeyType type, string key)
-            {
-                string value = type == KeyType.A ? DataAccess.GetValueForKeyA(key) : DataAccess.GetValueForKeyB(key);
-                await AddKeyToCache(cache, key, value);
-
-                return value;
-            }
-            public static async Task AddKeyToCache(IDistributedCache cache, string key, string value)
-            {
-                var options = new DistributedCacheEntryOptions();
-                options.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(60);
-
-                await cache.SetStringAsync(key, value, options);
-            }
-
+            return await AddKeyToCacheAndReturn(cache, type, inputKey);
         }
-        internal static class StringHelper
+        public static async Task<string> AddKeyToCacheAndReturn(IDistributedCache cache, KeyType type, string key)
         {
-            internal static string AddPrefix(KeyType type, string key)
-            {
-                return GetPrefix(type) + key;
-            }
-            internal static string GetPrefix(KeyType type)
-            {
-                return type == KeyType.A ? "a_" : "b_";
-            }
+            string value = type == KeyType.A ? DataAccess.GetValueForKeyA(key) : DataAccess.GetValueForKeyB(key);
+            await AddKeyToCache(cache, key, value);
+
+            return value;
         }
-        internal static class DataAccess
+        public static async Task AddKeyToCache(IDistributedCache cache, string key, string value)
         {
-            internal static string GetValueForKeyA(string key)
-            {
-                return StringHelper.GetPrefix(KeyType.B) + key;
-            }
-            internal static string GetValueForKeyB(string key)
-            {
-                return StringHelper.GetPrefix(KeyType.A) + key;
-            }
+            var options = new DistributedCacheEntryOptions();
+            options.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(60);
+
+            await cache.SetStringAsync(key, value, options);
         }
-        public enum KeyType
+
+    }
+    internal static class StringHelper
+    {
+        internal static string AddPrefix(KeyType type, string key)
         {
-            A,
-            B
+            return GetPrefix(type) + key;
         }
+        internal static string GetPrefix(KeyType type)
+        {
+            return type == KeyType.A ? "a_" : "b_";
+        }
+    }
+    internal static class DataAccess
+    {
+        internal static string GetValueForKeyA(string key)
+        {
+            return StringHelper.GetPrefix(KeyType.B) + key;
+        }
+        internal static string GetValueForKeyB(string key)
+        {
+            return StringHelper.GetPrefix(KeyType.A) + key;
+        }
+    }
+    public enum KeyType
+    {
+        A,
+        B
     }
 }
